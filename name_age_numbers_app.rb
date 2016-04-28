4171d8d0a382e0cae83931c28524a0d3485ca0c3
Aaron Hill <aaronthill16@gmail.com>
2016-04-28 18:21:50 -0400

    Refactored to make global variables work

--- name_age_numbers_app.rb
+++ name_age_numbers_app.rb
@@ -1,17 +1,17 @@
 require 'sinatra'
-@name = ""
+
 get '/' do
 	erb :get_name
 end
 
 post '/input' do
-	@name = params[:user_name]
-	erb :get_age, :locals => {:name => @name}
+	@@name = params[:user_name]
+	erb :get_age, :locals => {:name => @@name}
 end
 
 post '/age' do
-	@age = params[:user_age]
-	erb :get_favorite_numbers, :locals => {:age => @age, :name => @name}
+	@@age = params[:user_age]
+	erb :get_favorite_numbers, :locals => {:age => @@age, :name => @@name}
 end
 
 post '/fav_nums' do
@@ -19,10 +19,10 @@ post '/fav_nums' do
 	fav_num2 = params[:user_favorite_number_two]
 	fav_num3 = params[:user_favorite_number_three]
 	sum = fav_num1.to_i + fav_num2.to_i + fav_num3.to_i
-	if sum < @age
-		erb :display_comment, :locals => {:sum => sum, :fav_num1 => fav_num1, :fav_num2 => fav_num2, :fav_num3 => fav_num3, :name => @name, :less => "less"}
-	elsif sum > @age
-		erb :display_comment, :locals => {:sum => sum, :fav_num1 => fav_num1, :fav_num2 => fav_num2, :fav_num3 => fav_num3, :name => @name, :more => "more"}
+	if sum < @@age.to_i
+		erb :display_comment, :locals => {:sum => sum, :fav_num1 => fav_num1, :fav_num2 => fav_num2, :fav_num3 => fav_num3, :name => @@name, :result => "less"}
+	elsif sum > @@age.to_i
+		erb :display_comment, :locals => {:sum => sum, :fav_num1 => fav_num1, :fav_num2 => fav_num2, :fav_num3 => fav_num3, :name => @@name, :result => "more"}
 	end
 end
 
